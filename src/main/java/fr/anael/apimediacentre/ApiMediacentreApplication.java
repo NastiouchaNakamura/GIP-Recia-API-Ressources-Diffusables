213fa0115b03db9cea535f64ed97a3763238f39f
Anaël iMac <anael@barodine.fr>
2021-04-15 10:22:49 +0200

    Ajout projet médiacentre.

---
+++ src/main/java/fr/anael/apimediacentre/ApiMediacentreApplication.java
@@ -0,0 +1,11 @@
+package fr.anael.apimediacentre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMediacentreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiMediacentreApplication.class, args);
+    }
+}
---
+++ src/test/java/fr/anael/apimediacentre/ApiMediacentreApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.anael.apimediacentre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMediacentreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
